a60ab2af59e0c0b2fc213f51ee6a3a264ef3f203
Stu Brown <sbrown@cloudbees.com>
2020-08-14 09:27:21 +0100

    Addin in uth

--- scripts/create_master/create_master.sh
+++ scripts/create_master/create_master.sh
@@ -2,7 +2,7 @@ reqMasterName=$1
 USER=$2
 TOKEN=$3
 CJOC_URL=https://core.general.flow-training.beescloud.com/cjoc
-sed "s/MASTERNAME/$reqMasterName/" createManagedMasterK8s.groovy.template > createManagedMasterK8s.groovy
+sed "s/MASTERNAME/$reqMasterName/" scripts/create_master/createManagedMasterK8s.groovy.template > scripts/create_master/createManagedMasterK8s.groovy
 curl -v -XPOST \
     -u $USER:$TOKEN \
     --data-urlencode "script=$(<scripts/create_master/createManagedMasterK8s.groovy)" \
